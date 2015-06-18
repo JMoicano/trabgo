c9b792bd125fd7a450bfb161f1d3b7a8dd284fe7
Cesar Henrique Bernabe <cesar-hb@hotmail.com>
2015-06-17 22:17:09 -0300

    Autor e Revisor prontinhons

--- src/revisor.go
+++ src/revisor.go
@@ -1,9 +1,10 @@
-package main
+package revisor
 
 import (
 	"fmt"
 	"strings"
-	//"strconv"
+	"strconv"
+	"bytes"
 )
 
 type Revisor struct {
@@ -17,30 +18,30 @@ type Revisor struct {
 	notasAtribuidas float64
 }
 
-func criarRevisor(nome string, email string, senha int, instituicao string, endereco string) Revisor {
+func CriarRevisor(nome string, email string, senha int, instituicao string, endereco string) Revisor {
 	return (Revisor{nome, email, senha, instituicao, endereco, nil, 0, 0.0})
 }
 
-func toString(s Revisor) string {
+func ToString(s Revisor) string {
 	return s.nome
 }
 
-func addTema(r Revisor, tema string) {
+func AddTema(r Revisor, tema string) {
 	r.temas = append(r.temas, tema)
 }
 
-func adicionaRevisao(r Revisor, media float64) Revisor{
+func AdicionaRevisao(r Revisor, media float64) Revisor{
 	r.notasAtribuidas = r.notasAtribuidas + media
 	r.artigosRevisados++
 
 	return r
 }
 
-func isEnvolvido(r Revisor) bool {
+func IsEnvolvido(r Revisor) bool {
 	return r.artigosRevisados > 0
 }
 
-func isApto(r Revisor, tema string) bool {
+func IsApto(r Revisor, tema string) bool {
 	for _, v := range r.temas {
 		if(strings.EqualFold(tema, v)){
 			return true
@@ -50,17 +51,18 @@ func isApto(r Revisor, tema string) bool {
 	return false
 }
 
-// func relatorioRevisor(r Revisor) string {
-// 	media := r.notasAtribuidas/float64(r.artigosRevisados)
-// 	temp := r.nome + ";" + string(r.artigosRevisados) + ";" + strconv.FormatFloat(media, 'E', 2, 0)
+func RelatorioRevisor(r Revisor) string {
+	var buffer bytes.Buffer
 
-// 	return temp
-// }
+	media := r.notasAtribuidas/float64(r.artigosRevisados)
 
-func main(){
+	media_string := strconv.FormatFloat(media, 'f', 2, 64)
 
-	r := criarRevisor("Cesar", "cesar@email", 123, "ufes", "endereco")
-	r = adicionaRevisao(r, 9)
+	buffer.WriteString(r.nome)
+	buffer.WriteString(";")
+	buffer.WriteString(strconv.Itoa(r.artigosRevisados))
+	buffer.WriteString(";")
+	buffer.WriteString(media_string)
 
-	fmt.Println(r)
+	return buffer.String()
 }
