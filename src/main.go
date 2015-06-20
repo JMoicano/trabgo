1bf6857a7c4c4cb3a1a9049f7d0da0c4049a0ba5
JMoicano <jmariossilva@gmail.com>
2015-06-20 20:37:00 -0300

    modularizaçao

--- src/main.go
+++ src/main.go
@@ -9,91 +9,33 @@ import(
 	"./revista"
 )
 
-func main() {
-	edFileName := flag.String("e", "edicao.txt", "Nome do arquivo contendo os dados da edição")
-	temFileName := flag.String("t", "temas.csv", "Nome do arquivo contendo os dados dos temas")
-	pesFileName := flag.String("p", "pessoas.csv", "Nome do arquivo contendo os dados das pessoas")
-	artFileName := flag.String("a", "artigos.csv", "Nome do arquivo contendo os dados dos artigos")
-	revFileName := flag.String("r", "edicao.txt", "Nome do arquivo contendo os dados das revisões")
-	flag.Parse()
-	
-	var chefe string
-	var edicao Edicao
-	var temas map[int]string
-	var autores map[int]Autor
-	var revisores map[int]Revisor
-	
-	pesFile, err := os.Open(*pesFileName)
+func readCSVFile(fileName string, fildNumber int)([][]string, err error){
+	file, err := os.Open(fileName)
 	if err != nil{
 		fmt.Println("Erro de I/O")
-		return
-	}
-	
-	defer pesFile.Close()
-	
-	pesReader := csv.NewReader(pesFile)
-	
-	pesReader.Comma = ';'
-	pesReader.FieldsPerRecord = 7
-	pesReader.TrimLeadingSpace = true
-	
-	rawPesCSVData, err := pesReader.ReadAll()
-	
-	if err != nil{
-		fmt.Println(err)
 		os.Exit(1)
 	}
 	
-	rawPesCSVData = rawPesCSVData[1:]
+	defer file.Close()
 	
-	for _,pessoa := range rawPesCSVData{
-		if pessoa[6] == A{
-			autores[pessoa[0]] = CriarAutor(pessoa[1], pessoa[2], pessoa[3], pessoa[4], pessoa[5])
-		}else{
-			revisores[pessoa[0]] = CriarRevisor(pessoa[1], pessoa[2], pessoa[3], pessoa[4], pessoa[5])
-		}
-	}
+	feader := csv.NewReader(file)
 	
-	temFile, err := os.Open(*temFileName)
-	if err != nil{
-		fmt.Println("Erro de I/O")
-		return
-	}
+	reader.Comma = ';'
+	reader.FieldsPerRecord = fildNumber
+	reader.TrimLeadingSpace = true
 	
-	defer temFile.Close()
-	
-	temReader := csv.NewReader(temFile)
-	
-	temReader.Comma = ';'
-	temReader.FieldsPerRecord = 3
-	temReader.TrimLeadingSpace = true
-	
-	rawTemCSVData, err := temReader.ReadAll()
+	rawData, err := reader.ReadAll()
 	
 	if err != nil{
 		fmt.Println(err)
-		os.Exit(1)
+		return nil, err
 	}
 	
-	rawTemCSVData = rawTemCSVData[1:]
-	
-	for _,tema := range rawTemCSVData{
-		index,_ := strconv.ParseInt(tema[0], 10, 0)
-		temas[int(index)] = tema[1]
-		revHabilitados := strings.Split(tema[2], ",")
-		for _, v := range revHabilitados{
-			cod,_ := strconv.ParseInt(strings.Trim(v, " "), 10, 0)
-			revisores[int(cod)].AddTema(tema[1])
-		}
-	}
-	
-	edFile, err := os.Open(*edFileName)
-	edReader := bufio.NewReader(edFile)
-	tema ,_ := edReader.ReadString('\n')
-	chefe,_ = edReader.ReadString('\n')
-	vol,_ = edReader.ReadString('\n')
-	num,_ = edReader.ReadString('\n')
-	dataStr,_ = edReader.ReadString('\n') 
+	rawData = rawPesCSVData[1:]
+	return rawData, nil
+}
+
+func parseData(dataStr string)(Time, error){
 	dataSplit := strings.Split(dataStr, "/")//TODO: pegar a data a partir do formato da data
 
 	//http://golang.org/pkg/time/#Parse
@@ -129,9 +71,47 @@ func main() {
 
 	data = data + dataSplit[0]
 
-	t, _ = time.Parse(dataLayout, data)
+	return time.ParseInLocation(dataLayout, data, &localLoc)
+}
+
+func main() {
+	var edFileName, temFileName, pesFileName, artFileName, revFileName string
+	edFileName = flag.String("e", "edicao.txt", "Nome do arquivo contendo os dados da edição")
+	temFileName = flag.String("t", "temas.csv", "Nome do arquivo contendo os dados dos temas")
+	pesFileName = flag.String("p", "pessoas.csv", "Nome do arquivo contendo os dados das pessoas")
+	artFileName = flag.String("a", "artigos.csv", "Nome do arquivo contendo os dados dos artigos")
+	revFileName = flag.String("r", "edicao.txt", "Nome do arquivo contendo os dados das revisões")
+	flag.Parse()
 	
-	fmt.Println(rawEdData)
+	var chefe string
+	var edicao Edicao
+	var temas map[int]string
+	var autores map[int]Autor
+	var revisores map[int]Revisor
+	
+	rawPesCSVData, _ := readCSVFile(pesFileName, 7)
+	
+	for _,pessoa := range rawPesCSVData{
+		if pessoa[6] == A{
+			autores[pessoa[0]] = CriarAutor(pessoa[1], pessoa[2], pessoa[3], pessoa[4], pessoa[5])
+		}else{
+			revisores[pessoa[0]] = CriarRevisor(pessoa[1], pessoa[2], pessoa[3], pessoa[4], pessoa[5])
+		}
+	}
+	
+	rawTemCSVData, _ := readCSVFile(temFileName, 3)
+	
+	for _,tema := range rawTemCSVData{
+		index,_ := strconv.ParseInt(tema[0], 10, 0)
+		temas[int(index)] = tema[1]
+		revHabilitados := strings.Split(tema[2], ",")
+		for _, v := range revHabilitados{
+			cod,_ := strconv.ParseInt(strings.Trim(v, " "), 10, 0)
+			revisores[int(cod)].AddTema(tema[1])
+		}
+	}
+	
+	edFile, err := os.Open(*edFileName)
 	
 	if err != nil{
 		fmt.Println("Erro de I/O")
@@ -139,6 +119,17 @@ func main() {
 	}
 	
 	defer edFile.Close()
+	
+	edReader := bufio.NewReader(edFile)
+	tema ,_ := edReader.ReadString('\n')
+	chefe,_ = edReader.ReadString('\n')
+	vol,_ = edReader.ReadString('\n')
+	num,_ = edReader.ReadString('\n')
+	dataStr,_ = edReader.ReadString('\n') 
+	data, _ := parseData(dataStr)
+	
+	fmt.Println(rawEdData)
+	
 
 	//fmt.Println(e)
 }
