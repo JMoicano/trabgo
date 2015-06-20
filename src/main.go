09304d2cc455394616467264caa215f679948112
JMoicano <jmariossilva@gmail.com>
2015-06-20 18:32:32 -0300

    Main

--- src/main.go
+++ src/main.go
@@ -1,11 +1,89 @@
 package main
 
 import(
+	"os"
+	"fmt"
+	"encoding/csv"
+	"flag"
 	"./revista"
 )
 
 func main() {
-	e := CriarEdicao(1, 2, time.Now())
+	edFileName := flag.String("e", "edicao.txt", "Nome do arquivo contendo os dados da edição")
+	temFileName := flag.String("t", "temas.csv", "Nome do arquivo contendo os dados dos temas")
+	pesFileName := flag.String("p", "pessoas.csv", "Nome do arquivo contendo os dados das pessoas")
+	artFileName := flag.String("a", "artigos.csv", "Nome do arquivo contendo os dados dos artigos")
+	revFileName := flag.String("r", "edicao.txt", "Nome do arquivo contendo os dados das revisões")
+	flag.Parse()
+	
+	var temas map[int]String
+	var autores map[int]Autor
+	var revisores map[int]Revisor
+	
+	pesFile, err := os.Open(*pesFileName)
+	if err != nil{
+		fmt.Println("Erro de I/O")
+		return
+	}
+	
+	defer pesFile.Close()
+	
+	pesReader := csv.NewReader(pesFile)
+	
+	pesReader.Comma = ';'
+	pesReader.FieldsPerRecord = 7
+	pesReader.TrimLeadingSpace = true
+	
+	rawPesCSVData, err := pesReader.ReadAll()
+	
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	
+	rawPesCSVData = rawPesCSVData[1:]
+	
+	for _,pessoa := range rawPesCSVData{
+		if pessoa[6] == A{
+			autores[pessoa[0]] = CriarAutor(pessoa[1], pessoa[2], pessoa[3], pessoa[4], pessoa[5])
+		}else{
+			revisores[pessoa[0]] = CriarRevisor(pessoa[1], pessoa[2], pessoa[3], pessoa[4], pessoa[5])
+		}
+	}
+	
+		temFile, err := os.Open(*temFileName)
+	if err != nil{
+		fmt.Println("Erro de I/O")
+		return
+	}
+	
+	defer temFile.Close()
+	
+	temReader := csv.NewReader(temFile)
+	
+	temReader.Comma = ';'
+	temReader.FieldsPerRecord = 3
+	temReader.TrimLeadingSpace = true
+	
+	rawTemCSVData, err := temReader.ReadAll()
+	
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	
+	rawTemCSVData = rawTemCSVData[1:]
+	
+	for _,tema := range rawTemCSVData{
+		index,_ := strconv.ParseInt(tema[0], 10, 0)
+		temas[int(index)] = tema[1]
+		revHabilitados := strings.Split(tema[2], ",")
+		for _, v := range revHabilitados{
+			cod,_ := strconv.ParseInt(strings.Trim(v, " "), 10, 0)
+			fmt.Println(cod)
+		}
+	}
+	//e := CriarEdicao(1, 2, time.Now())
 
-	fmt.Println(e)
+	//fmt.Println(e)
 }
