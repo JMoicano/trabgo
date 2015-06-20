9efda597d57ae0248a84bec4773d3755e0924aff
JMoicano <jmariossilva@gmail.com>
2015-06-20 14:00:59 -0300

    terminei o artigo

--- src/artigo.go
+++ src/artigo.go
@@ -2,25 +2,42 @@ package artigo
 
 import (
 	"fmt"
-//	"autor"
+	"autor"
+	"revisor"
 )
 
 type Artigo struct{
 	titulo string
-//	contato Autor
-//	listaAutores[] Autor
-//	listaRevisores[] Revisor
+	contato *Autor
+	listaAutores[] *Autor
+	listaRevisores[] *Revisor
 	media float64
 	revisoesEnviadas int
 }
 
-/*func (art *Artigo) AdicionaAutor(autor *Autor){
-	art.listaAutores.append(autor);
+func CriarArtigo(titulo string, contato *Autor) Artigo{
+	var a Autor
+	a.titulo = titulo
+	a.contato = contato
+	a.AdicionaAutor(contato)
+	a.media = 0
+	a.revisoesEnviadas = 0
+	return a
 }
 
-func (art *Artigo) AdicionaRevisao(media float64, autor *Autor){
-	
-}*/
+func (art *Artigo) AdicionaAutor(autor *Autor){
+	art.listaAutores = append(art.listaAutores, autor)
+}
+
+func (art *Artigo) AdicionaRevisao(media float64, revisor *Revisor){
+	art.listaRevisores = append(art.listaRevisores, revisor)
+	revisor.AdicionaRevisao(media)
+	art.media += media
+	art.revisoesEnviadas ++
+	if art.revisoesEnviadas == 3{
+		art.media /= 3
+	}
+}
 
 func (art Artigo) GetRevisoesEnviadas() int{
 	return revisoesEnviadas
